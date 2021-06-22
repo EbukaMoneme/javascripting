baa5454a85d1c59466e8c674ea46ac923233c2e2
EbukaMoneme <camoneme@gmail.com>
2021-06-22 22:43:14 +0000

    Initial commit with all my work so far for javascripting

---
+++ if-statements.js
@@ -0,0 +1,6 @@
+let fruit = "orange";
+ans = fruit.length;
+if (ans > 5) {
+console.log("The fruit name has more than five characters.")}
+else {
+console.log("The fruit name has less than five characters.")}; 
